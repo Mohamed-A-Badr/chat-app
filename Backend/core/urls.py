c98bdfa801153ec9f51dbcc9c71707d7734086d9
Mohamed-A-Badr <badr49960@gmail.com>
2025-02-20 02:45:32 +0200

    Update files

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("chat.urls")),
     re_path(r"^auth/", include("djoser.urls")),
     re_path(r"^auth/", include("djoser.urls.jwt")),
 ]
