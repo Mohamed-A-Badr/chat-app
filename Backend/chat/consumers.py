b6ba20fcd2f8f42dc7ba3d682e87fb8309d8122e
Mohamed-A-Badr <badr49960@gmail.com>
2025-02-20 03:32:58 +0200

    Add channel layer to broadcast messages for multiple users

--- Backend/chat/consumers.py
+++ Backend/chat/consumers.py
@@ -5,15 +5,28 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.name = self.scope["url_route"]["kwargs"]["chat_name"]
+        self.chat_name = f"chat_{self.name}"
+
+        await self.channel_layer.group_add(self.chat_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_discard(self.chat_name, self.channel_name)
 
     async def receive(self, text_data=None):
-        message = "Empty Json"
-        if text_data:
-            json_data = json.loads(text_data)
-            message = json_data["message"]
+        json_text = json.loads(text_data)
+        message = json_text["message"]
+
+        await self.channel_layer.group_send(
+            self.chat_name,
+            {
+                "type": "chat.message",
+                "message": message,
+            },
+        )
+
+    async def chat_message(self, event):
+        message = event["message"]
 
         await self.send(text_data=json.dumps({"message": message}))
--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -148,3 +148,11 @@ sentry_sdk.init(
 )
 
 ASGI_APPLICATION = "core.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
