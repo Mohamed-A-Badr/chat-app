eaa251bc6e70d0a31395173482e0cc90ee792808
Mohamed-A-Badr <badr49960@gmail.com>
2025-03-08 21:32:36 +0200

    Implement endpoints for user profile and user list

--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -40,3 +40,12 @@ class LogoutSerializer(serializers.Serializer):
     refresh = serializers.CharField()
 
     default_error_messages = {"bad_token": ("Token is expired or invalid")}
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "username",
+            "email",
+        )
